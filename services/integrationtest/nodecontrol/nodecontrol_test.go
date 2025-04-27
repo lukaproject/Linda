c42cc9529c0b2fb48208c9e9f85a3d6a3f83a5d5
dxyinme <dxyinme@outlook.com>
2025-04-27 20:56:38 +0800

    remove filesaver code, add integration test

--- services/integrationtest/nodecontrol/nodecontrol_test.go
+++ services/integrationtest/nodecontrol/nodecontrol_test.go
@@ -4,6 +4,7 @@ import (
 	"Linda/baselibs/apiscall/swagger"
 	"Linda/services/integrationtest/stage"
 	"fmt"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
@@ -28,6 +29,28 @@ func (s *nodeControlTestSuite) TestNormalScenario() {
 	currentStage.NodeOperations.FreeNode(testNodeId)
 	<-currentStage.WaitForNodeFree(testNodeId)
 	currentStage.DeleteBag(testBagName)
+}
+
+func (s *nodeControlTestSuite) TestReJoinDifferentPoolFailed() {
+	conf := swagger.NewConfiguration()
+	conf.BasePath = fmt.Sprintf("http://localhost:%d/api", stage.AgentCentralPort)
+	cli := swagger.NewAPIClient(conf)
+
+	currentStage := &stage.Stage{}
+	currentStage.SetUp(s.T(), cli)
+	testBagName := currentStage.CreateBag("test-current-bag")
+	testBagName2 := currentStage.CreateBag("test-current-bag-2")
+
+	defer func() {
+		currentStage.DeleteBag(testBagName)
+		currentStage.DeleteBag(testBagName2)
+	}()
+
+	testNodeId := currentStage.SelectOneNodeJoinToBag(testBagName)
+	s.T().Log("join bag finished")
+	s.Equal(http.StatusConflict, currentStage.NodeOperations.JoinBagWithStatusCode(testBagName2, testNodeId))
+	currentStage.NodeOperations.FreeNode(testNodeId)
+	<-currentStage.WaitForNodeFree(testNodeId)
 }
 
 func TestNodeControlTestSuiteMain(t *testing.T) {
