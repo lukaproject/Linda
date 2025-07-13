f3cff91776915606aadae7606b95b14c1b2f09a6
dxyinme <dxyinme@outlook.com>
2025-07-13 11:31:08 +0800

    use docker push action

--- tools/builder/build_base_image.sh
+++ tools/builder/build_base_image.sh
@@ -2,17 +2,11 @@
 
 set -e
 
-PUSH=$1
-PROXY=$2
+PROXY=$1
 image="ghcr.io/lukaproject/linda/linda-buildbase-image:latest"
 dockerfilePath="tools/dockerimages/buildbase/Dockerfile.buildbase"
 
-echo "build image $image, push $PUSH"
+echo "build image $image"
 echo "dockerfile $dockerfilePath"
 
 docker buildx build -f $dockerfilePath -t $image . --build-arg PROXY=$PROXY
-
-if [[ $PUSH = true ]]; then
-    echo "docker push $image"
-    docker push $image
-fi
