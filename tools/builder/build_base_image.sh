3bc069aea69d5cbd0f9cb6fea1adf719f1d8fe6b
dxyinme <dxyinme@outlook.com>
2025-07-13 11:13:16 +0800

    add build base build

---
+++ tools/builder/build_base_image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+$PUSH=$1
+$PROXY=$2
+$image="ghcr.io/lukaproject/linda/linda-buildbase-image:latest"
+$dockerfilePath="tools/dockerimages/buildbase/Dockerfile.buildbase"
+
+echo "build image $image, push $PUSH"
+echo "dockerfile $dockerfilePath"
+
+docker buildx build -f $dockerfilePath -t $image . --build-arg PROXY=$PROXY
+
+if [[ $PUSH = true ]]; then
+    echo "docker push $image"
+    docker push $image
+fi
